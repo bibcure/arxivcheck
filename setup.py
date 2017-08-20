c36c8beb844d0edafe5a5a4a6d98508f70377407
bruno_messias <messias.physics@gmail.com>
2017-08-20 16:55:17 -0300

    Update README.md

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 from setuptools import setup, find_packages
 
-readme = open('README.md','r')
-README_TEXT = readme.read()
-readme.close()
 
 setup(
     name="arxivcheck",
-    version="0.1.4",
+    version="0.1.5",
     packages = find_packages(exclude=["build",]),
     scripts=["arxivcheck/bin/arxivcheck"],
-    long_description = README_TEXT,
     install_requires=["future", "feedparser", "bibtexparser", "doitobib"],
     include_package_data=True,
     license="GPLv3",
     description="Generate a bibtex given a arxiv id or title, check if published",
     author="Bruno Messias",
     author_email="messias.physics@gmail.com",
-    download_url="https://github.com/bibcure/arxivcheck/archive/0.1.4.tar.gz",
+    download_url="https://github.com/bibcure/arxivcheck/archive/0.1.5.tar.gz",
     keywords=["bibtex","arxiv", "science","scientific-journals"],
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
