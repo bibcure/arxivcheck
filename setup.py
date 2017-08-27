a650edc48118d32f571d212ff4574534e538ea78
bruno_messias <messias.physics@gmail.com>
2017-08-27 19:13:11 -0300

    update infos

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="arxivcheck",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(exclude=["build", ]),
     scripts=["arxivcheck/bin/arxivcheck"],
     install_requires=["future","unidecode", "feedparser", "bibtexparser", "doi2bib"],
@@ -12,7 +12,7 @@ setup(
     description="Generate a bibtex given a arxiv id or title, check if published",
     author="Bruno Messias",
     author_email="messias.physics@gmail.com",
-    download_url="https://github.com/bibcure/arxivcheck/archive/0.3.0.tar.gz",
+    download_url="https://github.com/bibcure/arxivcheck/archive/0.3.1.tar.gz",
     keywords=["bibtex", "arxiv", "science", "scientific-journals"],
     classifiers=[
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
