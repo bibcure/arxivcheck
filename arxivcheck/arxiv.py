c3908c5305e7d06a86dacff03edd2fb7e40d96ae
Dominic Else <dominic.else@gmail.com>
2018-11-08 16:38:07 -0500

    fix bug: always get the right arXiv ID even when searching by title

--- arxivcheck/arxiv.py
+++ arxivcheck/arxiv.py
@@ -136,7 +136,8 @@ def check_arxiv_published(value, field="id", get_first=True, keep_eprint=False):
             doi = item["arxiv_doi"]
             published, bib = get_bib_from_doi(doi)
             if keep_eprint:
-                bib = add_eprint_to_bib(bib, value)
+                eprint = re.split('/|v',item["id"])[-2]
+                bib = add_eprint_to_bib(bib, eprint)
         else:
             bib = generate_bib_from_arxiv(item, value, field)
     else:
